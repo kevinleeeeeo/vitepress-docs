25a97a9d99ee647a42ea785a9305a346382de078
大田 <kevinlee1225@hotmail.com>
2023-03-27 16:11:29 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,6 @@
 # abort on errors
 set -e
 
-node -v
-
-
 # build
 npm run docs:build
 
@@ -16,17 +13,17 @@ cd docs/.vitepress/dist
 # if you are deploying to a custom domain
 # echo 'study.dt-code.fun' > CNAME
 
-# git init
-# git add -A
-# git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 # just a example, please change the follow line with your github account!!!
-#  git push -f https://gitee.com/kevinleeeee/vitepress-docs-site.git main:gh-pages
+ git push -f https://github.com/kevinlee-coder/vitepress-docs.git main:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 ``
 cd -
-# rm -rf docs/.vitepress/dist
+rm -rf docs/.vitepress/dist
